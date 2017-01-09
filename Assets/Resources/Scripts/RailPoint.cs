89e69a8521c1fc5e21d491b429ee83f2edd55f0a
keitanxkeitan <keitanxkeitan@gmail.com>
2017-01-10 01:01:03 +0900

    最初のコミット

---
+++ Assets/Resources/Scripts/RailPoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RailPoint : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnDrawGizmos() {
+		Gizmos.color = Color.green;
+		Gizmos.DrawCube (transform.position, Vector3.one);
+	}
+}
